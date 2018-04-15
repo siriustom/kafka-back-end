01e67eda9346c1100a9b67c2029bc524c7a7ae08
siriustom <linxiaoran0210@gmail.com>
2018-04-15 15:21:57 -0700

    add back consumer offsetoutofrange after fix bug not getting message after first message

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ console.log('kafka back-end server is running');
 consumer.on('error', function (err) {
     console.log(err);
 });
-// consumer.on('offsetOutOfRange', function (topic) {
-//     topic.maxNum = 2;
-//     var offset = connection.getOffset();
-//     offset.fetch([topic], function (err, offsets) {
-//         if (err) {
-//             return console.error(err);
-//         }
-//         var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
-//         consumer.setOffset(topic.topic, topic.partition, min);
-//     });
-// });
+consumer.on('offsetOutOfRange', function (topic) {
+    topic.maxNum = 2;
+    var offset = connection.getOffset();
+    offset.fetch([topic], function (err, offsets) {
+        if (err) {
+            return console.error(err);
+        }
+        var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+        consumer.setOffset(topic.topic, topic.partition, min);
+    });
+});
 
 consumer.on('message', function (message) {
     console.log('message received', message);
