4a9fe66cda1a84583c0cef2deba7fca423fcc387
siriustom <linxiaoran0210@gmail.com>
2018-04-14 22:23:02 -0700

    implement handle_postproject function

--- services/postProjectService.js
+++ services/postProjectService.js
@@ -1,8 +1,16 @@
-var User = require('../models/user');
+var Project = require('../models/project');
 var producerService = require('./producerService');
 
 function handle_postproject(content) {
-
+    Project.createProject(content, function (err, project) {
+        if (err) throw err;
+        if (!project) {
+            var noUser = {message: 'no user'};
+            return producerService.producerSend(content, noUser);
+        }
+        var success = {message: 'project posted'};
+        return producerService.producerSend(content, success);
+    })
 }
 
 exports.handle_postproject = handle_postproject;
