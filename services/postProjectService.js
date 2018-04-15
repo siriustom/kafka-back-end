e385bbcca157aa80ed02b4106db267b20ea90557
Ubuntu <ubuntu@ip-172-31-22-246.us-west-2.compute.internal>
2018-04-15 06:42:09 +0000

    delete no use code

--- services/postProjectService.js
+++ services/postProjectService.js
@@ -4,10 +4,6 @@ var producerService = require('./producerService');
 function handle_postproject(content) {
     Project.createProject(content, function (err, project) {
         if (err) throw err;
-        if (!project) {
-            var noUser = {message: 'no user'};
-            return producerService.producerSend(content, noUser);
-        }
         var success = {message: 'project posted'};
         return producerService.producerSend(content, success);
     })
